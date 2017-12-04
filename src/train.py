00bdd93265ed06000fc70a59ac0431592b15e4ea
Shin Kibin <bindung@linecorp.com>
2017-12-04 11:05:37 +0900

    Apply formatter

--- src/train.py
+++ src/train.py
@@ -70,140 +70,135 @@ mode = learn.ModeKeys.TRAIN  # 'Configure' training mode for dropout layers
 
 
 def _get_input():
-    """Set up and return image, label, and image width tensors"""
-
-    image, width, label, _, _, _ = mjsynth.bucketed_input_pipeline(
-        FLAGS.train_path,
-        str.split(FLAGS.filename_pattern, ','),
-        batch_size=FLAGS.batch_size,
-        num_threads=FLAGS.num_input_threads,
-        input_device=FLAGS.input_device,
-        width_threshold=FLAGS.width_threshold,
-        length_threshold=FLAGS.length_threshold)
-
-    # tf.summary.image('images',image) # Uncomment to see images in TensorBoard
-    return image, width, label
+  """Set up and return image, label, and image width tensors"""
+  
+  image, width, label, _, _, _ = \
+    mjsynth.bucketed_input_pipeline(FLAGS.train_path,
+                                    str.split(FLAGS.filename_pattern, ','),
+                                    batch_size=FLAGS.batch_size,
+                                    num_threads=FLAGS.num_input_threads,
+                                    input_device=FLAGS.input_device,
+                                    width_threshold=FLAGS.width_threshold,
+                                    length_threshold=FLAGS.length_threshold)
+  
+  # tf.summary.image('images',image) # Uncomment to see images in TensorBoard
+  return image, width, label
 
 
 def _get_single_input():
-    """Set up and return image, label, and width tensors"""
-
-    image, width, label, length, text, filename = mjsynth.threaded_input_pipeline(
-        deps.get('records'),
-        str.split(FLAGS.filename_pattern, ','),
-        batch_size=1,
-        num_threads=FLAGS.num_input_threads,
-        num_epochs=1,
-        batch_device=FLAGS.input_device,
-        preprocess_device=FLAGS.input_device)
-    return image, width, label, length, text, filename
+  """Set up and return image, label, and width tensors"""
+  
+  image, width, label, length, text, filename = \
+    mjsynth.threaded_input_pipeline(deps.get('records'),
+                                    str.split(FLAGS.filename_pattern, ','),
+                                    batch_size=1,
+                                    num_threads=FLAGS.num_input_threads,
+                                    num_epochs=1,
+                                    batch_device=FLAGS.input_device,
+                                    preprocess_device=FLAGS.input_device)
+  return image, width, label, length, text, filename
 
 
 def _get_training(rnn_logits, label, sequence_length):
-    """Set up training ops"""
-    with tf.name_scope("train"):
-
-        if FLAGS.tune_scope:
-            scope = FLAGS.tune_scope
-        else:
-            scope = "convnet|rnn"
-
-        rnn_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
-                                     scope=scope)
-
-        loss = model.ctc_loss_layer(rnn_logits, label, sequence_length)
-
-        # Update batch norm stats [http://stackoverflow.com/questions/43234667]
-        extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-
-        with tf.control_dependencies(extra_update_ops):
-
-            learning_rate = tf.train.exponential_decay(
-                FLAGS.learning_rate,
-                tf.train.get_global_step(),
-                FLAGS.decay_steps,
-                FLAGS.decay_rate,
-                staircase=FLAGS.decay_staircase,
-                name='learning_rate')
-
-            optimizer = tf.train.AdamOptimizer(
-                learning_rate=learning_rate,
-                beta1=FLAGS.momentum)
-
-            train_op = tf.contrib.layers.optimize_loss(
-                loss=loss,
-                global_step=tf.train.get_global_step(),
-                learning_rate=learning_rate,
-                optimizer=optimizer,
-                variables=rnn_vars)
-
-            tf.summary.scalar('learning_rate', learning_rate)
-
-    return train_op
+  """Set up training ops"""
+  with tf.name_scope("train"):
+    if FLAGS.tune_scope:
+      scope = FLAGS.tune_scope
+    else:
+      scope = "convnet|rnn"
+    
+    rnn_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+                                 scope=scope)
+    
+    loss = model.ctc_loss_layer(rnn_logits, label, sequence_length)
+    
+    # Update batch norm stats [http://stackoverflow.com/questions/43234667]
+    extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    
+    with tf.control_dependencies(extra_update_ops):
+      learning_rate = tf.train.exponential_decay(FLAGS.learning_rate,
+                                                 tf.train.get_global_step(),
+                                                 FLAGS.decay_steps,
+                                                 FLAGS.decay_rate,
+                                                 staircase=FLAGS.decay_staircase,
+                                                 name='learning_rate')
+      
+      optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate,
+                                         beta1=FLAGS.momentum)
+      
+      train_op = tf.contrib.layers.optimize_loss(loss=loss,
+                                                 global_step=tf.train.get_global_step(),
+                                                 learning_rate=learning_rate,
+                                                 optimizer=optimizer,
+                                                 variables=rnn_vars)
+      
+      tf.summary.scalar('learning_rate', learning_rate)
+  return train_op
 
 
 def _get_session_config():
-    """Setup session config to soften device placement"""
-
-    config = tf.ConfigProto(
-        allow_soft_placement=True,
-        log_device_placement=False)
-
-    return config
+  """Setup session config to soften device placement"""
+  
+  config = tf.ConfigProto(allow_soft_placement=True,
+                          log_device_placement=False)
+  
+  return config
 
 
 def _get_init_pretrained():
-    """Return lambda for reading pretrained initial model"""
-
-    if not FLAGS.tune_from:
-        return None
-
-    saver_reader = tf.train.Saver(
-        tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES))
-
-    ckpt_path = FLAGS.tune_from
-
-    init_fn = lambda sess: saver_reader.restore(sess, ckpt_path)
-
-    return init_fn
+  """Return lambda for reading pretrained initial model"""
+  
+  if not FLAGS.tune_from:
+    return None
+  
+  saver_reader = tf.train.Saver(
+    tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES))
+  
+  ckpt_path = FLAGS.tune_from
+  
+  init_fn = lambda sess: saver_reader.restore(sess, ckpt_path)
+  
+  return init_fn
 
 
 def main(argv=None):
-    with tf.Graph().as_default():
-        global_step = tf.contrib.framework.get_or_create_global_step()
-
-        image, width, label = _get_input()
-
-        with tf.device(FLAGS.train_device):
-            features, sequence_length = model.convnet_layers(image, width, mode)
-            logits = model.rnn_layers(features,
-                                      sequence_length,
-                                      mjsynth.num_classes())
-            train_op = _get_training(logits, label, sequence_length)
-
-        session_config = _get_session_config()
-
-        summary_op = tf.summary.merge_all()
-        init_op = tf.group(tf.global_variables_initializer(),
-                           tf.local_variables_initializer())
-
-        sv = tf.train.Supervisor(
-            logdir=FLAGS.output,
-            init_op=init_op,
-            summary_op=summary_op,
-            save_summaries_secs=60,
-            init_fn=_get_init_pretrained(),
-            save_model_secs=1800)
-
-        with sv.managed_session(config=session_config) as sess:
-            step = sess.run(global_step)
-            while step < FLAGS.max_num_steps:
-                if sv.should_stop():
-                    break
-                [step_loss, step] = sess.run([train_op, global_step])
-            sv.saver.save(sess, os.path.join(FLAGS.output, 'model.ckpt'),
-                          global_step=global_step)
+  with tf.Graph().as_default():
+    global_step = tf.contrib.framework.get_or_create_global_step()
+    
+    image, width, label = _get_input()
+    
+    with tf.device(FLAGS.train_device):
+      features, sequence_length = model.convnet_layers(image, width, mode)
+      logits = model.rnn_layers(features,
+                                sequence_length,
+                                mjsynth.num_classes())
+      train_op = _get_training(logits, label, sequence_length)
+    
+    session_config = _get_session_config()
+    
+    summary_op = tf.summary.merge_all()
+    init_op = tf.group(tf.global_variables_initializer(),
+                       tf.local_variables_initializer())
+    
+    sv = tf.train.Supervisor(logdir=FLAGS.output,
+                             init_op=init_op,
+                             summary_op=summary_op,
+                             save_summaries_secs=60,
+                             init_fn=_get_init_pretrained(),
+                             save_model_secs=1800)
+    
+    with sv.managed_session(config=session_config) as sess:
+      step = sess.run(global_step)
+      while step < FLAGS.max_num_steps:
+        if sv.should_stop():
+          break
+        [step_loss, step] = sess.run([train_op, global_step])
+        break;
+      
+      sv.saver.save(sess,
+                    os.path.join(FLAGS.output, 'model.ckpt'),
+                    global_step=global_step)
 
 
 if __name__ == '__main__':
-    tf.app.run()
+  tf.app.run()
